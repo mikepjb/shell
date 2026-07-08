4ff47ea50f85dbd05e42548a48c79006282c7dde
Mike Bruce <mike@hypalynx.com>
2026-07-08 08:24:06 +0100

    more emacs setup

--- config/init.el
+++ config/init.el
@@ -1,5 +1,33 @@
 (setq gc-cons-threshold (* 64 1024 1024))
 
+;; TODO are we able to set this first and have the function/macro
+;; definitions later on? and then call (+setup) at the end of the
+;; file?
+;; (defun +setup ()
+;;   "Configures Emacs so it is ready for action!")
+
+(defmacro +setm (&rest modes)
+  "Sets modes according to the MODES interpreted as pairs using seq-partition."
+  (let (body)
+    (dolist (m (seq-partition modes 2))
+      (push `(,(car m) ,(cadr m)) body))
+    `(progn ,@(nreverse body))))
+
+
+;; ;; repls
+;; (defun +repl (&optional arg)
+;;   (interactive "P")
+;;   (let* ((repls '(("clojure" inferior-lisp "clojure -A:dev")
+;;                   ("python" run-python)
+;;                   ("sqlite" sql-sqlite)
+;;                   ("node" comint-run "node")
+;;                   ("ruby" comint-run "irb")))
+;;          (spec (cdr (assoc (if arg (completing-read "REPL: " repls nil t)
+;;                              "clojure")
+;;                            repls))))
+;;     (other-window-prefix)
+;;     (apply (car spec) (cdr spec))))
+
 (dolist (k '(mac-command-modifier x-super-keysym))
   (when (boundp k) (set k 'meta)))
 
@@ -7,31 +35,29 @@
 
 (load-theme 'modus-vivendi t) ;; or tango-dark / wombat
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(blink-cursor-mode -1)
-(electric-pair-mode 1)
-(show-paren-mode 1)
-(savehist-mode 1)
-(save-place-mode 1)
-(global-auto-revert-mode 1)
-(fido-mode 1)
-
-(defun +setm (&rest modes)
-  "Sets modes according to the MODES interpreted as tuples."
-  (dolist (m (seq-partition modes 2))
-    ;; (message (car m) (cdr m))
-    (funcall (car m) (cdr m))))
-
 (+setm
- menu-bar-mode 1
- tool-bar-mode -1)
+ menu-bar-mode -1
+ tool-bar-mode -1
+ scroll-bar-mode -1
+ blink-cursor-mode -1
+ electric-pair-mode 1
+ show-paren-mode 1
+ savehist-mode 1
+ save-place-mode 1
+ global-auto-revert-mode 1
+ fido-mode 1)
 
 (setq
  inhibit-startup-screen t
  ring-bell-function 'ignore
  )
+
+(+setr
+ "clojure" '(inferior-lisp "clojure -A:dev")
+ "python" run-python
+ "sqlite" sql-sqlite
+ "node" '(comint-run "node")
+ "ruby" '(comint-run "irb"))
 
 (dolist (b `(
 	     ("M-s" save-buffer)
@@ -45,7 +71,6 @@
 	     ))
   (global-set-key (kbd (car b)) (cadr b)))
 
-;; line numbers (code only)
 ;; C-w (maybe another way?)
 ;; paredit
 ;; REPLs! (incl. sql! clojure/racket (for SICP)/python/jshell)
